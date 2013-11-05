f5e20432818b291568f0b84f68f6c5d4737b153f
Chisel <chisel@chizography.net>
2013-11-05 10:35:34 +0000

    Import of CHISEL/Test-DBIx-Class-Schema-1.0.6 from CPAN.
    gitpan-cpan-distribution: Test-DBIx-Class-Schema
    gitpan-cpan-version:      1.0.6
    gitpan-cpan-path:         CHISEL/Test-DBIx-Class-Schema-1.0.6.tar.gz
    gitpan-cpan-author:       CHISEL
    gitpan-cpan-maturity:     released

--- lib/Test/DBIx/Class/Schema.pm
+++ lib/Test/DBIx/Class/Schema.pm
@@ -1,6 +1,6 @@
 package Test::DBIx::Class::Schema;
 {
-  $Test::DBIx::Class::Schema::VERSION = '1.0.5';
+  $Test::DBIx::Class::Schema::VERSION = '1.0.6';
 }
 {
   $Test::DBIx::Class::Schema::DIST = 'Test-DBIx-Class-Schema';
@@ -300,7 +300,7 @@ Test::DBIx::Class::Schema - DBIx::Class schema sanity checking tests
 
 =head1 VERSION
 
-version 1.0.5
+version 1.0.6
 
 =head1 SYNOPSIS
 
